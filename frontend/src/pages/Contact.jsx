7e43916cefef6806f21b46783d7368e5fa17bfb3
lsj2015-hub <lsj2015@hotmail.com>
2024-09-27 22:15:08 +0800

    Contact Page Design completed

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,5 +1,41 @@
+import { assets } from '../assets/assets';
+import NewsLetterBox from '../components/NewsLetterBox';
+import Title from '../components/Title';
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1="CONTACT" text2="US" />
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img
+          src={assets.contact_img}
+          alt=""
+          className="w0full md:max-w-[480px]"
+        />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">
+            54709 willms Station <br /> Suite 350, Washington, USA
+          </p>
+          <p className="text-gray-500">
+            Tel: (415) 555-0132 <br /> Email: admin@forever.com
+          </p>
+          <p className="font-semibold text-xl text-gray-600">
+            Career at Forever
+          </p>
+          <p className="text-gray-500">
+            Learn more about our teams and job openings.
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            EXplore Jobs
+          </button>
+        </div>
+      </div>
+      <NewsLetterBox />
+    </div>
+  );
 };
 
 export default Contact;
