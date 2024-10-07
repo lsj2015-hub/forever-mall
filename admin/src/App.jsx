75ea0291ed681e63faa7b0fb632e6a269d3336fc
lsj2015-hub <lsj2015@hotmail.com>
2024-10-07 15:57:01 +0800

    Create Admin Dashboard

---
+++ admin/src/App.jsx
@@ -0,0 +1,40 @@
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Add from './pages/Add';
+import List from './pages/List';
+import Orders from './pages/Orders';
+import { useState } from 'react';
+import Login from './components/Login';
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+const App = () => {
+  const [token, setToken] = useState('');
+
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      {token === '' ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                {/* Add routes here */}
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
