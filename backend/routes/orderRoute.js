b262bc0b984dc0b0289212bf6c9b6bcbf4cbd319
lsj2015-hub <lsj2015@hotmail.com>
2024-10-10 17:26:37 +0800

    Stripe Payment

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -6,6 +6,7 @@ import {
   allOrders,
   updateStatus,
   userOrders,
+  verifyStripe,
 } from '../controllers/orderController.js';
 import adminAuth from '../middleware/adminAuth.js';
 import authUser from '../middleware/auth.js';
@@ -23,5 +24,8 @@ orderRouter.post('/razorpay', authUser, placeOrderRazorpay);
 
 // User Features
 orderRouter.post('/userorders', authUser, userOrders);
+
+// verify payment
+orderRouter.post('/verifyStripe', authUser, verifyStripe);
 
 export default orderRouter;
